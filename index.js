3b05452dceb650e1f6bd8f75394d35d7f8e9cda5
ray-ray <rayturla21@yahoo.com>
2022-10-22 14:55:15 +0800

    Import web3 functions for transactions

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const {
   clusterApiUrl,
   Keypair,
   LAMPORTS_PER_SOL,
+  Transaction,
+  SystemProgram,
+  sendAndConfirmRawTransaction,
+  sendAndConfirmTransaction,
 } = require("@solana/web3.js");
 
 // Connect to devnet
